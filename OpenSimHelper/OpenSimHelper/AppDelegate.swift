ee67d233d5c62aefe90875d87c706cce95a69f41
LUO Sheng <luosheng1986@gmail.com>
2016-10-11 13:53:24 +0800

    Add OpenSim helper app.

---
+++ OpenSimHelper/OpenSimHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OpenSimHelper
+//
+//  Created by Luo Sheng on 11/10/2016.
+//  Copyright © 2016 Pop Tap. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OpenSimHelper/OpenSimHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OpenSimHelper
+//
+//  Created by Luo Sheng on 11/10/2016.
+//  Copyright © 2016 Pop Tap. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
