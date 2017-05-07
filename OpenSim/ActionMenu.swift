ea2fcf657c4045295aa8f90f7bf5145717306f9c
Luo Sheng <luosheng1986@gmail.com>
2017-05-07 10:24:03 +0800

    Add reveal action

--- OpenSim/ActionMenu.swift
+++ OpenSim/ActionMenu.swift
@@ -17,11 +17,28 @@ final class ActionMenu: NSMenu {
         self.device = device
         self.application = application
         super.init(title: "")
+        
+        buildActions()
     }
     
     required init(coder decoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func buildActions() {
+        let revealInFinderAction = NSMenuItem(title: "Reveal Sandbox in Finder", action: #selector(revealInFinder(sender:)), keyEquivalent: "")
+        revealInFinderAction.target = self
+        self.addItem(revealInFinderAction)
+    }
+    
+    @objc private func revealInFinder(sender: AnyObject) {
+        guard let application = application,
+            let URL = device?.containerURLForApplication(application),
+            FileManager.default.fileExists(atPath: URL.path) else {
+                return
+        }
+        
+        NSWorkspace.shared().open(URL)
+    }
     
 }
