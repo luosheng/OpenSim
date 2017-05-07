3b83d4d8ca9635949947a1f5740ac22bf54a0d1c
Luo Sheng <luosheng1986@gmail.com>
2017-05-07 15:10:09 +0800

    Add app info view

---
+++ OpenSim/AppInfoView.swift
@@ -0,0 +1,13 @@
+//
+//  AppInfoView.swift
+//  OpenSim
+//
+//  Created by Luo Sheng on 07/05/2017.
+//  Copyright © 2017 Luo Sheng. All rights reserved.
+//
+
+import Cocoa
+
+final class AppInfoView: NSView {
+    
+}
