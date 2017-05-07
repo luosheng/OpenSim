7a4b147fa17cc8deea845916349ab0b004730480
Luo Sheng <luosheng1986@gmail.com>
2017-05-07 14:10:04 +0800

    Add app menu item

---
+++ OpenSim/AppMenuItem.swift
@@ -0,0 +1,13 @@
+//
+//  AppMenuItem.swift
+//  OpenSim
+//
+//  Created by Luo Sheng on 07/05/2017.
+//  Copyright © 2017 Luo Sheng. All rights reserved.
+//
+
+import Cocoa
+
+class AppMenuItem: NSMenuItem {
+    
+}
