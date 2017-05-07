a60422ac3fc703539c229d8ee844e6b29322a04f
Luo Sheng <luosheng1986@gmail.com>
2017-05-07 10:38:13 +0800

    Add copy to path action

--- OpenSim/ActionMenu.swift
+++ OpenSim/ActionMenu.swift
@@ -14,9 +14,25 @@ final class ActionMenu: NSMenu {
     weak var application: Application?
     
     var revealInFinderMenuItem: NSMenuItem {
-        let item = NSMenuItem(title: "Reveal Sandbox in Finder", action: #selector(revealInFinder(sender:)), keyEquivalent: "")
+        let item = NSMenuItem(title: "Reveal Sandbox in Finder", action: #selector(revealInFinder(_:)), keyEquivalent: "")
         item.target = self
         return item
+    }
+    
+    var copyPathMenuItem: NSMenuItem {
+        let item = NSMenuItem(title: "Copy Sandbox Path to Pasteboard", action: #selector(copyToPasteboard(_:)), keyEquivalent: "")
+        item.target = self
+        return item
+    }
+    
+    private var sandboxUrl: URL? {
+        guard let application = application,
+            let url = device?.containerURLForApplication(application),
+            FileManager.default.fileExists(atPath: url.path)
+            else {
+                return nil
+        }
+        return url
     }
     
     init(device: Device, application: Application) {
@@ -25,20 +41,22 @@ final class ActionMenu: NSMenu {
         super.init(title: "")
         
         self.addItem(revealInFinderMenuItem)
+        self.addItem(copyPathMenuItem)
     }
     
     required init(coder decoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    @objc private func revealInFinder(sender: AnyObject) {
-        guard let application = application,
-            let URL = device?.containerURLForApplication(application),
-            FileManager.default.fileExists(atPath: URL.path) else {
-                return
+    @objc private func revealInFinder(_ sender: AnyObject) {
+        if let url = sandboxUrl {
+            NSWorkspace.shared().open(url)
         }
-        
-        NSWorkspace.shared().open(URL)
     }
     
+    @objc private func copyToPasteboard(_ sender: AnyObject) {
+        if let url = sandboxUrl {
+            NSPasteboard.general().setString(url.path, forType: NSPasteboardTypeString)
+        }
+    }
 }
