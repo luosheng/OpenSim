9c4a5507e61366577ce594b06e32d83e4ad1bb7b
Luo Sheng <luosheng1986@gmail.com>
2017-05-07 14:08:16 +0800

    Add icons
    Icon made by Vaadin[https://vaadin.com/font-icons] from www.flaticon.com

--- OpenSim/ActionMenu.swift
+++ OpenSim/ActionMenu.swift
@@ -21,24 +21,28 @@ final class ActionMenu: NSMenu {
     
     private var revealInFinderItem: NSMenuItem? {
         let item = NSMenuItem(title: "Reveal Sandbox in Finder", action: #selector(revealInFinder(_:)), keyEquivalent: "")
+        item.image = #imageLiteral(resourceName: "reveal")
         item.target = self
         return item
     }
     
     private var copyPathItem: NSMenuItem? {
         let item = NSMenuItem(title: "Copy Sandbox Path to Pasteboard", action: #selector(copyToPasteboard(_:)), keyEquivalent: "")
+        item.image = #imageLiteral(resourceName: "share")
         item.target = self
         return item
     }
     
     private var openInTerminalItem: NSMenuItem? {
         let item = NSMenuItem(title: "Open Sandbox in Terminal", action: #selector(openInTerminal(_:)), keyEquivalent: "")
+        item.image = #imageLiteral(resourceName: "terminal")
         item.target = self
         return item
     }
     
     private var uninstallItem: NSMenuItem? {
         let item = NSMenuItem(title: "Uninstall…", action: #selector(uninstall(_:)), keyEquivalent: "")
+        item.image = #imageLiteral(resourceName: "uninstall")
         item.target = self
         return item
     }
