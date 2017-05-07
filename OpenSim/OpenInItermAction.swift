f5d5c9d8b00bfa244b6d6c0c2c98cdb2ec83ea91
Luo Sheng <luosheng1986@gmail.com>
2017-05-07 21:46:28 +0800

    Add an action to open sandbox in iTerm

--- OpenSim/OpenInItermAction.swift
+++ OpenSim/OpenInItermAction.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2017 Luo Sheng. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+class OpenInItermAction: ApplicationActionable {
+    
+    private let iTermPath = NSWorkspace.shared().absolutePathForApplication(withBundleIdentifier: "com.googlecode.iterm2")
+    
+    let title = NSLocalizedString("Open Sandbox in iTerm", comment: "")
+    
+    var icon: NSImage? {
+        guard let path = iTermPath else {
+            return nil
+        }
+        
+        let image = NSWorkspace.shared().icon(forFile: path)
+        image.size = NSSize(width: 16, height: 16)
+        return image
+    }
+    
+    var isAvailable: Bool {
+        return iTermPath != nil
+    }
+    
+    func perform(with application: Application) {
+        if let url = application.sandboxUrl {
+            NSWorkspace.shared().openFile(url.path, withApplication: "iTerm")
+        }
+    }
+    
+}
