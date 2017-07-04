538c039e4f98fa38a29ee242e4f208bf0efa7700
LUO Sheng <luosheng1986@gmail.com>
2017-07-04 10:34:14 +0800

    Build menu asynchronously

--- OpenSim/SimulatorController.swift
+++ OpenSim/SimulatorController.swift
@@ -14,15 +14,16 @@ struct SimulatorController {
     static func uninstall(_ application: Application) {
         _ = shell("/usr/bin/xcrun", arguments: ["simctl", "uninstall", application.device.UDID, application.bundleID])
     }
-    
-    static func listDevices() -> [Runtime] {
+
+    static func listDevices(callback: ([Runtime]) -> ()) {
         guard let jsonString = shell("/usr/bin/xcrun", arguments: ["simctl", "list", "-j", "devices"]),
             let data = jsonString.data(using: String.Encoding.utf8),
             let json = try? JSONSerialization.jsonObject(with: data, options:[]) as? [String: AnyObject],
             let devicesJson = json?["devices"] as? [String:AnyObject] else {
-                return []
+                callback([])
+                return
         }
-        
+
         var runtimes = [Runtime]()
         for (runtimeName, deviceList) in devicesJson {
             let runtime = Runtime(name: runtimeName)
@@ -33,7 +34,7 @@ struct SimulatorController {
                         let name = deviceJson["name"],
                         let udid = deviceJson["udid"] {
                         let device = Device(udid: udid, type: name, name: name, state: state, availability: availability)
-                        
+
                         if device.availability == .available {
                             runtime.devices.append(device)
                         }
@@ -45,9 +46,9 @@ struct SimulatorController {
             }
             runtimes.append(runtime)
         }
-        
+
         let filteredRuntime = runtimes.filter { $0.name.contains("iOS") && $0.devices.count > 0 }
-        
-        return filteredRuntime
+
+        callback(filteredRuntime)
     }
 }
